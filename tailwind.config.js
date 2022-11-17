f51065cdca0d10f9e55c0571dbb5422c0f36ebc3
briziomusic <briziomail@gmail.com>
2022-11-17 12:18:52 +0100

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,41 @@
+module.exports = {
+  content: ['./src/**/*.{vue,js,ts}'],
+  plugins: [require('daisyui')],
+
+  // daisyUI config (optional)
+  daisyui: {
+    styled: true,
+    themes: true,
+    base: true,
+    utils: true,
+    logs: true,
+    rtl: false,
+    prefix: "",
+    darkTheme: "dark",
+    themes: [
+      {
+        mytheme: {
+        
+          "primary": "#01965e",
+                  
+          "secondary": "#01965e",
+                  
+          "accent": "#01965e",
+                  
+          "neutral": "#eee",
+                  
+          "base-100": "#2D384E",
+                  
+          "info": "#71B2DA",
+                  
+          "success": "#105644",
+                  
+          "warning": "#F19D37",
+                  
+          "error": "#F34960",
+        },
+      },
+    ]
+  },
+
+};
