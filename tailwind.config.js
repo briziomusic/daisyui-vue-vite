450fc82d9bc5eb8c1b546e70a68f5a99da574c59
briziomusic <briziomail@gmail.com>
2022-11-24 12:53:59 +0100

    3 files modified, 3 added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,15 @@
-const defaultTheme = require("tailwindcss/defaultTheme");
+const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = {
-  content: ["./src/**/*.{vue,js,ts}"],
-  plugins: [require("@tailwindcss/typography"), require("daisyui")],
+  content: ['./src/**/*.{vue,js,ts}'],
+  plugins: [require('@tailwindcss/typography'), require('daisyui')],
 
   theme: {
     extend: {
       fontFamily: {
-        sans: ['"BNPP Sans"', "system-ui"],
-        serif: ['"BNPP Sans"', "Georgia"],
-        mono: ['"BNPP Sans"', "SFMono-Regular"],
+        sans: ['bnpSans', 'sans-serif'],
+        serif: ['"BNPP Sans"', 'Georgia'],
+        mono: ['"BNPP Sans"', 'SFMono-Regular'],
         display: ['"BNPP Sans"'],
         body: ['"BNPP Sans"'],
       },
@@ -24,41 +24,19 @@ module.exports = {
     utils: true,
     logs: true,
     rtl: false,
-    prefix: "",
-    darkTheme: "dark",
+    prefix: '',
+    darkTheme: 'dark',
 
     themes: [
       {
-        mytheme: {
-          primary: "#01965e",
-
-          secondary: "#01965e",
-
-          accent: "#01965e",
-
-          neutral: "#eee",
-
-          "base-100": "#2D384E",
-
-          info: "#71B2DA",
-
-          success: "#105644",
-
-          warning: "#F19D37",
-
-          error: "#F34960",
-          fontFamily: {
-            sans: ['"BNPP Sans"', "system-ui"],
-            serif: ['"BNPP Sans"', "Georgia"],
-            mono: ['"BNPP Sans"', "SFMono-Regular"],
-            display: ['"BNPP Sans"'],
-            body: ['"BNPP Sans"'],
-          },
+        tntt_light: {
+          ...require('daisyui/src/colors/themes')['[data-theme=corporate]'],
+          
+          'base-content': '#2d2926',
         },
       },
-      "light",
-      "dark",
+      'light',
+      'dark',
     ],
-    
   },
 };
